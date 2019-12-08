ab4faf8686d570b5c346475ed4ea6e3bde725b26
matheusrocha89 <matheus.rocha89@gmail.com>
2019-12-08 20:45:10 +0000

    Refactor component to PureComponent to avoid performance issues

--- src/components/ExpensesInfo/ExpensesInfo.js
+++ src/components/ExpensesInfo/ExpensesInfo.js
@@ -1,105 +1,108 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, PureComponent} from 'react';
 import {gql} from 'apollo-boost';
 import {Card, CardItem, Text} from 'native-base';
 
 import {formatToCurrency, formatToBrazilianDate} from '../../utils';
 import {textStyles} from '../../styles';
 
-export const ExpensesInfo = ({expense}) => {
-  const expenseDateFormatted = formatToBrazilianDate(expense.dataDocumento);
+export class ExpensesInfo extends PureComponent {
+  render() {
+    const {expense} = this.props;
+    const expenseDateFormatted = formatToBrazilianDate(expense.dataDocumento);
 
-  return (
-    <Card>
-      <CardItem header bordered>
-        <Text>{expense.tipoDespesa}</Text>
-      </CardItem>
+    return (
+      <Card>
+        <CardItem header bordered>
+          <Text>{expense.tipoDespesa}</Text>
+        </CardItem>
 
-      <CardItem>
-        <Text style={textStyles.cardInfoLabel}>CPF/CNPJ:</Text>
-      </CardItem>
-      <CardItem>
-        <Text>{expense.cnpjCpfFornecedor}</Text>
-      </CardItem>
+        <CardItem>
+          <Text style={textStyles.cardInfoLabel}>CPF/CNPJ:</Text>
+        </CardItem>
+        <CardItem>
+          <Text>{expense.cnpjCpfFornecedor}</Text>
+        </CardItem>
 
-      <CardItem>
-        <Text style={textStyles.cardInfoLabel}>Tipo de documento:</Text>
-      </CardItem>
-      <CardItem>
-        <Text>{expense.tipoDocumento}</Text>
-      </CardItem>
+        <CardItem>
+          <Text style={textStyles.cardInfoLabel}>Tipo de documento:</Text>
+        </CardItem>
+        <CardItem>
+          <Text>{expense.tipoDocumento}</Text>
+        </CardItem>
 
-      <CardItem>
-        <Text style={textStyles.cardInfoLabel}>Número do documento:</Text>
-      </CardItem>
-      <CardItem>
-        <Text>{expense.numDocumento}</Text>
-      </CardItem>
+        <CardItem>
+          <Text style={textStyles.cardInfoLabel}>Número do documento:</Text>
+        </CardItem>
+        <CardItem>
+          <Text>{expense.numDocumento}</Text>
+        </CardItem>
 
-      <CardItem>
-        <Text style={textStyles.cardInfoLabel}>Data:</Text>
-      </CardItem>
-      <CardItem>
-        <Text>{expenseDateFormatted}</Text>
-      </CardItem>
+        <CardItem>
+          <Text style={textStyles.cardInfoLabel}>Data:</Text>
+        </CardItem>
+        <CardItem>
+          <Text>{expenseDateFormatted}</Text>
+        </CardItem>
 
-      <CardItem>
-        <Text style={textStyles.cardInfoLabel}>Fornecedor: </Text>
-      </CardItem>
-      <CardItem>
-        <Text>{expense.nomeFornecedor}</Text>
-      </CardItem>
+        <CardItem>
+          <Text style={textStyles.cardInfoLabel}>Fornecedor: </Text>
+        </CardItem>
+        <CardItem>
+          <Text>{expense.nomeFornecedor}</Text>
+        </CardItem>
 
-      {!!expense.numRessarcimento && (
-        <Fragment>
-          <CardItem>
-            <Text style={textStyles.cardInfoLabel}>
-              Número de ressarcimento:
-            </Text>
-          </CardItem>
-          <CardItem>
-            <Text>{expense.numRessarcimento}</Text>
-          </CardItem>
-        </Fragment>
-      )}
+        {!!expense.numRessarcimento && (
+          <Fragment>
+            <CardItem>
+              <Text style={textStyles.cardInfoLabel}>
+                Número de ressarcimento:
+              </Text>
+            </CardItem>
+            <CardItem>
+              <Text>{expense.numRessarcimento}</Text>
+            </CardItem>
+          </Fragment>
+        )}
 
-      {expense.parcela > 0 && (
-        <Fragment>
-          <CardItem>
-            <Text style={textStyles.cardInfoLabel}>Parcela:</Text>
-          </CardItem>
-          <CardItem>
-            <Text>{expense.parcela}</Text>
-          </CardItem>
-        </Fragment>
-      )}
+        {expense.parcela > 0 && (
+          <Fragment>
+            <CardItem>
+              <Text style={textStyles.cardInfoLabel}>Parcela:</Text>
+            </CardItem>
+            <CardItem>
+              <Text>{expense.parcela}</Text>
+            </CardItem>
+          </Fragment>
+        )}
 
-      <CardItem>
-        <Text style={textStyles.cardInfoLabel}>Valor do documento:</Text>
-      </CardItem>
-      <CardItem>
-        <Text>{`R$ ${formatToCurrency(expense.valorDocumento)}`}</Text>
-      </CardItem>
+        <CardItem>
+          <Text style={textStyles.cardInfoLabel}>Valor do documento:</Text>
+        </CardItem>
+        <CardItem>
+          <Text>{`R$ ${formatToCurrency(expense.valorDocumento)}`}</Text>
+        </CardItem>
 
-      {expense.valorGlosa > 0 && (
-        <Fragment>
-          <CardItem>
-            <Text style={textStyles.cardInfoLabel}>Valor glosa:</Text>
-          </CardItem>
-          <CardItem>
-            <Text>{`R$ ${formatToCurrency(expense.valorGlosa)}`}</Text>
-          </CardItem>
-        </Fragment>
-      )}
+        {expense.valorGlosa > 0 && (
+          <Fragment>
+            <CardItem>
+              <Text style={textStyles.cardInfoLabel}>Valor glosa:</Text>
+            </CardItem>
+            <CardItem>
+              <Text>{`R$ ${formatToCurrency(expense.valorGlosa)}`}</Text>
+            </CardItem>
+          </Fragment>
+        )}
 
-      <CardItem>
-        <Text style={textStyles.cardInfoLabel}>Valor líquido:</Text>
-      </CardItem>
-      <CardItem>
-        <Text>{`R$ ${formatToCurrency(expense.valorLiquido)}`}</Text>
-      </CardItem>
-    </Card>
-  );
-};
+        <CardItem>
+          <Text style={textStyles.cardInfoLabel}>Valor líquido:</Text>
+        </CardItem>
+        <CardItem>
+          <Text>{`R$ ${formatToCurrency(expense.valorLiquido)}`}</Text>
+        </CardItem>
+      </Card>
+    );
+  }
+}
 
 ExpensesInfo.fragments = {
   expensesInfo: gql`

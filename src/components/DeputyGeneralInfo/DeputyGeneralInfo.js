ead516c7f30d6fef264958ffcd04093b61a14746
matheusrocha89 <matheus.rocha89@gmail.com>
2019-12-08 19:17:22 +0000

    Formatting dates and styles on DeputyGeneralInfo component

--- src/components/DeputyGeneralInfo/DeputyGeneralInfo.js
+++ src/components/DeputyGeneralInfo/DeputyGeneralInfo.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {gql} from 'apollo-boost';
 import {Card, CardItem, Text} from 'native-base';
 
+import {formatToBrazilianDate} from '../../utils';
+import {textStyles} from '../../styles';
+
 export const DeputyGeneralInfo = ({deputy}) => {
+  const birthDateFormatted = formatToBrazilianDate(deputy.dataNascimento);
+  const deathDateFormatted = formatToBrazilianDate(deputy.dataFalecimento);
+
   return (
     <Card>
       <CardItem header bordered>
@@ -11,24 +17,44 @@ export const DeputyGeneralInfo = ({deputy}) => {
       <CardItem header>
         <Text>Dados Pessoais</Text>
       </CardItem>
+
       <CardItem>
-        <Text>{`CPF: ${deputy.cpf}`}</Text>
+        <Text styles={textStyles.cardInfoLabel}>CPF:</Text>
       </CardItem>
       <CardItem>
-        <Text>{`Data Nascimento: ${deputy.dataNascimento}`}</Text>
+        <Text>{deputy.cpf}</Text>
       </CardItem>
+
+      <CardItem>
+        <Text styles={textStyles.cardInfoLabel}>Data Nascimento:</Text>
+      </CardItem>
+      <CardItem>
+        <Text>{birthDateFormatted}</Text>
+      </CardItem>
+
       {deputy.dataFalecimento && (
-        <CardItem>
-          <Text>{`Data Falecimento: ${deputy.dataFalecimento}`}</Text>
-        </CardItem>
+        <Fragment>
+          <CardItem>
+            <Text styles={textStyles.cardInfoLabel}>Data Falecimento:</Text>
+          </CardItem>
+          <CardItem>
+            <Text>{deathDateFormatted}</Text>
+          </CardItem>
+        </Fragment>
       )}
+
       <CardItem>
-        <Text>{`Grau de escolaridade: ${deputy.escolaridade}`}</Text>
+        <Text styles={textStyles.cardInfoLabel}>Grau de escolaridade:</Text>
       </CardItem>
       <CardItem>
-        <Text>
-          {`Local de Nascimento: ${deputy.municipioNascimento} - ${deputy.ufNascimento}`}
-        </Text>
+        <Text>{deputy.escolaridade}</Text>
+      </CardItem>
+
+      <CardItem>
+        <Text style={textStyles.cardInfoLabel}>Local de Nascimento:</Text>
+      </CardItem>
+      <CardItem>
+        <Text>{`${deputy.municipioNascimento} - ${deputy.ufNascimento}`}</Text>
       </CardItem>
     </Card>
   );
